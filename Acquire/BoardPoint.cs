c4f4ae98f22fc05feebb03852215ab9a94928046
ophirbushi <ophirbushi@gmail.com>
2015-08-15 20:10:11 +0300

    Finished documentation of the BoardPoint class.

--- Acquire/BoardPoint.cs
+++ Acquire/BoardPoint.cs
@@ -1,41 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-//using System.Threading.Tasks;
 
 namespace Acquire
 {
+    /// <summary>
+    /// A point in the board. 
+    /// </summary>
     public struct BoardPoint
     {
-        public int X, Y;
+        /// <summary>
+        /// The X coordinate of the point.
+        /// </summary>
+        public readonly int X;
+
+        /// <summary>
+        /// The Y coordinate of the point.
+        /// </summary>
+        public readonly int Y;
+
+        /// <summary>
+        /// The list of the point's neighboring points (Also includes points that are beyond the board's borders).
+        /// </summary>
         public List<BoardPoint> Neighbors { get { return new List<BoardPoint> { Above(), Below(), Right(), Left() }; } }
+
+        /// <summary>
+        /// Create a new board point by specifying its X and Y coordinates.
+        /// </summary>
+        /// <param name="x">The X coordinate of the point.</param>
+        /// <param name="y">The Y coordinate of the point.</param>
         public BoardPoint(int x, int y)
         {
             X = x;
             Y = y;
         }
 
+        /// <summary>
+        /// Gets the point's neighboring board point from above.
+        /// </summary>
+        /// <returns>The point's neighboring board point from above.</returns>
         public BoardPoint Above()
         {
             return new BoardPoint(X, Y - 1);
         }
 
+        /// <summary>
+        /// Gets the point's neighboring board point from below.
+        /// </summary>
+        /// <returns>The point's neighboring board point from below.</returns>
         public BoardPoint Below()
         {
             return new BoardPoint(X, Y + 1);
         }
 
+        /// <summary>
+        /// Gets the point's neighboring board point to the right.
+        /// </summary>
+        /// <returns>The point's neighboring board point to the right.</returns>
         public BoardPoint Right()
         {
             return new BoardPoint(X + 1, Y);
         }
 
+        /// <summary>
+        /// Gets the point's neighboring board point to the left.
+        /// </summary>
+        /// <returns>The point's neighboring board point to the left.</returns>
         public BoardPoint Left()
         {
             return new BoardPoint(X - 1, Y);
         }
-
 
         public override string ToString()
         {
