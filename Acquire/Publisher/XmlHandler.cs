135d0aef584bab27f348b008adf72fb2f2cd8480
ophirbushi <ophirbushi@gmail.com>
2015-09-19 23:25:53 +0300

    Fix to earlier commit.

--- Acquire/Publisher/XmlHandler.cs
+++ Acquire/Publisher/XmlHandler.cs
@@ -19,10 +19,18 @@ namespace Acquire
             {
                 _xmlSerializer.Serialize(sw, gameState);
             }
+
+        }
+
+        public static GameState DeserializeGameState()
+        {
+            var path = Environment.CurrentDirectory + "/gamestate.xml";
+            GameState gameState;
             using (var sr = new StreamReader(path))
             {
-                GameState gs = _xmlSerializer.Deserialize(sr) as GameState;
+                gameState = _xmlSerializer.Deserialize(sr) as GameState;
             }
+            return gameState;
         }
     }
 }
